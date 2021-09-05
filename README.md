4f70005b97977c2549b58a22dbf9eea02b3fce9b
Tia Michaels <83481294+tmichae4@users.noreply.github.com>
2021-09-04 18:54:06 -1000

    API-Quiz

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # web-api-quiz
+
+A quiz that has multiple choice questions with a count down timer. 
+
+Quiz is built with:
+
+* HTML <br>
+* CSS <br>
+* JavaScript <br>
+
+Tia Michaels
---
+++ js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>", "<js>", "<script>", "<scripting>"],
+        answer: "<script>"
+    },
+    {
+        title: "Quiz Question 2",
+        choices: ["Correct", "2", "3", "4"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 3",
+        choices: ["1", "Correct", "3", "4"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 4",
+        choices: ["1", "2", "Correct", "4"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 5",
+        choices: ["1", "2", "3", "Correct"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 6",
+        choices: ["1", "2", "Correct", "4"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 7",
+        choices: ["1", "Correct", "3", "4"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 8",
+        choices: ["Correct", "2", "3", "4"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 9",
+        choices: ["1", "Correct", "3", "4"],
+        answer: "Correct"
+    },
+    {
+        title: "Quiz Question 10",
+        choices: ["1", "2", "Correct", "4"],
+        answer: "Correct"
+    }
+];
